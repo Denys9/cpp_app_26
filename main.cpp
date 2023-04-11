f9b729d2f432abb95c5c062a7f04597ba3a178f8
Denys9 <lozindenis6@gmail.com>
2023-04-11 18:28:45 +0300

    1

--- main.cpp
+++ main.cpp
@@ -4,21 +4,23 @@
 #include <string>
 using namespace std;
 
-int main() {
+int main() 
+{
 
-	string pathone = "ex1.txt";
+	string pathone = "one.txt";
 	fstream firstfile;
 	string line;
 	firstfile.open(pathone, ios::in);
 	if (firstfile.is_open()) {
-		while (getline(firstfile, line)) {
+		while (getline(firstfile, line)) 
+		{
 			cout << line << endl;
 
 		}
 		firstfile.close();
 	}
 	else {
-		cout << "File can`t open!" << endl;
+		cout << "file not open" << endl;
 	}
 
 	return 0;
